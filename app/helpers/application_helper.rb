af2cfed446289f1442bb99f8bf4ac9ace50c8682
Jorge Lim <jorgelim40@gmail.com>
2018-04-24 13:24:20 +0800

    Chapter 4: Rails-flavored Ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def display_title page_title=""
+  	base_title = "Ruby on Rails Chapter 3"
+    page_title = (page_title.empty? ? page_title : page_title + " | ") + base_title
+  end
 end
